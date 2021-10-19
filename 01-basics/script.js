7d20f3692ac8be198214aad398424fb64662bf7b
Johan Nordström <johan@digitalvillage.se>
2021-10-19 10:48:32 +0200

    Our first lines of JavaScript.

---
+++ 01-basics/script.js
@@ -0,0 +1,14 @@
+/*
+alert("Hello from script.js");
+alert("Hello from script.js");
+alert("Hello from script.js");
+*/
+
+// alert("Hello from script.js");
+// alert("Hello from script.js");
+// alert("Hello from script.js");
+
+// Shows a popup saying hi
+//alert("Hej");
+
+console.log("Hello console!");
