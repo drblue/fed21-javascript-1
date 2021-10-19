e2c57362afc4bf7630d5c09c3041e8e8e31f6b6b
Johan Nordström <johan@digitalvillage.se>
2021-10-19 12:01:11 +0200

    String concatenation, variables and constants.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,14 +1,40 @@
-/*
-alert("Hello from script.js");
-alert("Hello from script.js");
-alert("Hello from script.js");
-*/
-
-// alert("Hello from script.js");
-// alert("Hello from script.js");
-// alert("Hello from script.js");
-
 // Shows a popup saying hi
 //alert("Hej");
 
-console.log("Hello console!");
+/*
+let msg;
+msg = "Hej variabel";
+msg = "Hej igen variabel";
+
+// alert(msg);
+console.log(msg);
+
+msg = "Hej nytt meddelande";
+
+console.log(msg);
+*/
+
+/*
+let thisIsVariableNameVeryLong;   // camelCase
+
+let ThisIsVariableNameVeryLong // PascalCase   UpperCamelCase
+
+let first_name;
+let this_is_variable_name_very_long;   // snake_case
+*/
+
+let firstName; // first name
+firstName = "Johan";
+
+let helloMsg = "Hello " + firstName + "!";
+firstName = "Alicia";
+console.log(helloMsg);  // "Hello Johan!"
+
+helloMsg = "Hello " + "Alicia" + "!";  // "Hello Alicia!"
+console.log(helloMsg);  // ?
+
+const lastName = "Nordström";
+// lastName = "Testsson";  // NO CAN DO!
+
+const hello = "Hello";
+console.log(hello + " " + firstName + " " + lastName);
