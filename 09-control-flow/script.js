47aa5a77ba54f520941e74dccecce16405bb0c59
Johan Nordström <johan@digitalvillage.se>
2021-10-20 15:15:49 +0200

    Control Flow with if-statements.

--- 09-control-flow/script.js
+++ 09-control-flow/script.js
@@ -5,7 +5,7 @@
 const minAge = 18;
 const maxAge = 65;
 
-let age = 65;
+let age = 30;
 
 /**
  * Logical Operators
@@ -13,3 +13,29 @@ let age = 65;
  * && = AND (both has to be true)
  * || = OR (at least one has to be true)
  */
+
+//  30  >=  18    &&    30 <= 65
+//      true      &&      true
+if (age >= minAge && age <= maxAge) {
+	console.log(`You are between ${minAge} and ${maxAge} years old, congrats!`);
+} else {
+	console.log(`Too young or too old! 😝`);
+}
+
+console.log('After if expression');
+
+let username = "bob";
+if (username == "dave" || username == "johan") {
+	console.log("Access granted. Good morning!");
+} else {
+	console.log("I can't open the doors.");
+}
+
+// More advanced if-statement
+if (age >= minAge && age <= maxAge) {
+	console.log(`You are between ${minAge} and ${maxAge} years old, congrats!`);
+} else if (age < minAge) {
+	console.log(`Too young! 👶🏻`);
+} else if (age > maxAge) {
+	console.log(`Too old! 👴🏻`);
+}
