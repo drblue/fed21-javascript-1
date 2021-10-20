5cf53b3e25440e3fb0b056f26fd9e1406c1f3c81
Johan Nordström <johan@digitalvillage.se>
2021-10-20 14:40:57 +0200

    Comparisons and logical operators.

---
+++ 08-comparisons/script.js
@@ -0,0 +1,19 @@
+/**
+ * Comparisons
+ */
+
+const minAge = 18;
+const maxAge = 65;
+
+let age = 65;
+
+console.log("Is age greater than minAge?", age > minAge);               // 65 > 18? 👍🏻
+console.log("Is age greater than OR equal to minAge?", age >= minAge);  // 65 >= 18? 👍🏻
+
+console.log("Is age less than maxAge?", age < maxAge);                  // 65 < 65? 👎🏻
+
+console.log("Is age the same as maxAge?", age == maxAge);               // 65 == 65? 👴🏻
+console.log("Is age less than OR equal to maxAge?", age <= maxAge);     // 65 <= 65? 🥳
+
+console.log("Is age NOT the same as minAge?", age != minAge);           // 65 != 18? 👍🏻
+console.log("Is age NOT the same as maxAge?", age != maxAge);           // 65 != 65? 👎🏻
