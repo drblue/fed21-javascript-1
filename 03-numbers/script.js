934a1eb4da46fc1465dc67280d53a48553ac5753
Johan Nordström <johan@digitalvillage.se>
2021-10-20 09:39:26 +0200

    Numbers example.

---
+++ 03-numbers/script.js
@@ -0,0 +1,57 @@
+/*
+let width = 80;
+let height = 40;
+
+let circumference = width * 2 + height * 2;
+let area = width * height;
+console.log("Circumference of box is:", circumference);
+console.log("Area of box is:", area);
+*/
+
+/*
+let radius = 20;
+const pi = 3.14159;
+
+let circumference = 2 * pi * radius;
+console.log("Circumference of circle with radius " + radius + " is:", circumference);
+
+console.log("10 divided by 3 is:", 10 / 3);
+console.log("Modulus of 10 divided by 3:", 10 % 3);
+*/
+
+/*
+let points = 10;
+console.log("Points:", points);
+
+console.log("Points subtracted?", points--); // 10 (?!)
+// 9
+
+console.log("Points subtracted!", --points); // 8 🥳
+
+console.log("Points:", points);
+*/
+
+// math operators
+let score = 50;
+score = score + 5; // 55
+score = score - 15; // 40
+score = score * 2; // 80
+score = score / 4; // 20
+
+// shorthand math operators
+score = 50;
+score += 5; // 55
+score -= 15; // 40
+score *= 2; // 80
+score /= 4; // 20
+
+// NaN - Not a Number
+console.log("10 divided by 2:", 10 / 2); // 5
+console.log(5 + " points"); // "5 points"
+console.log("Likes: " + 2); // "Likes: 2"
+
+console.log("Posts" - 5); // 😳
+
+console.log("Batman" * 5); // ??
+
+console.log("Captain America" / 2);
