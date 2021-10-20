a95a611afe2b7a5bf4f3c3ec4aadcd25b0c150de
Johan Nordström <johan@digitalvillage.se>
2021-10-20 11:16:23 +0200

    Template Literals (Template Strings).

--- 06-template-strings/script.js
+++ 06-template-strings/script.js
@@ -7,10 +7,10 @@ let points = 42;
 // console.log(msg);
 
 // ES6 Template Literals (Template Strings)
-// let msg = `Hi ${student}, your result on ${test} was ${points} points.`;
-// console.log(msg);
+let msg = `Hi ${student}, your result on ${test} was ${points} points.`;
+console.log(msg);
 
-let html = `<article class="blog-article">
-	<h1>${heading}</h1>
-	<p>${content}</p>
-</article>`;
+// let html = `<article class="blog-article">
+// 	<h1>${heading}</h1>
+// 	<p>${content}</p>
+// </article>`;
