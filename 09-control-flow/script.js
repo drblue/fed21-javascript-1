67e188b23563793ef1ebe67f62ac3b6b35625c63
Johan Nordström <johan@digitalvillage.se>
2021-10-20 14:52:26 +0200

    Control Flow boilerplate.

---
+++ 09-control-flow/script.js
@@ -0,0 +1,15 @@
+/**
+ * Control Flow 👮🏽🎛✋🏻
+ */
+
+const minAge = 18;
+const maxAge = 65;
+
+let age = 65;
+
+/**
+ * Logical Operators
+ *
+ * && = AND (both has to be true)
+ * || = OR (at least one has to be true)
+ */
