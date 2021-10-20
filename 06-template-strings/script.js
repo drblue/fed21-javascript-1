2a48c216a1120ed6120ec4253c3f40698a96add1
Johan Nordström <johan@digitalvillage.se>
2021-10-20 11:13:16 +0200

    Template Literals (Template Strings).

---
+++ 06-template-strings/script.js
@@ -0,0 +1,16 @@
+let test = "JavaScript 101";
+let student = "Newbie Coder";
+let points = 42;
+
+// string concatenation
+// let msg = "Hi " + student + ", your result on " + test + " was " + points + " points.";
+// console.log(msg);
+
+// ES6 Template Literals (Template Strings)
+// let msg = `Hi ${student}, your result on ${test} was ${points} points.`;
+// console.log(msg);
+
+let html = `<article class="blog-article">
+	<h1>${heading}</h1>
+	<p>${content}</p>
+</article>`;
