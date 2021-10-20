176d9d6c56e53b4374fd8356e2702613c1bf324d
Johan Nordström <johan@digitalvillage.se>
2021-10-20 15:55:11 +0200

    For-loops.

--- 09-control-flow/script.js
+++ 09-control-flow/script.js
@@ -14,6 +14,7 @@ let age = 30;
  * || = OR (at least one has to be true)
  */
 
+/*
 //  30  >=  18    &&    30 <= 65
 //      true      &&      true
 if (age >= minAge && age <= maxAge) {
@@ -38,4 +39,36 @@ if (age >= minAge && age <= maxAge) {
 	console.log(`Too young! 👶🏻`);
 } else if (age > maxAge) {
 	console.log(`Too old! 👴🏻`);
+}
+*/
+
+/**
+ * for-loop
+ */
+/*
+console.log("Yey loop!");
+
+let maxLoops = 10;
+
+for (let i = 0; i < maxLoops; i++) {
+	// do stuff
+	console.log(i); // 4
+}
+
+// nu är vi snurriga av allt loopande
+console.log('🤢');
+*/
+
+let students = [
+	"Deadpool",     // 0
+	"Black Widow",  // 1
+	"Iron Man",     // 2
+	"Spider-Man",   // 3
+	"Wanda",        // 4
+	"Thanos",       // 5
+];
+
+for (let i = 0; i < students.length; i++) {
+	// this will run one time for each and every student
+	console.log(`Student at index ${i} is: ${students[i]}`);
 }
