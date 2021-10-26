ef7b18af5f568a8c6b2cd61644140e76bdcfce80
Johan Nordström <johan@digitalvillage.se>
2021-10-26 14:58:59 +0200

    Workshop: Password Check using Functions.

---
+++ 17-workshop-password-check-function/script.js
@@ -0,0 +1,27 @@
+/**
+ * Workshop: Password Checker using Functions
+ *
+ * Skriv om lösenordskollen till att använda funktioner (inkl. forEach) och
+ * kollar en lista av lösenord.
+ *
+ * Ni ska ha en funktion som tar emot ett lösenord och validerar det. Den ska
+ * returnera true om det är ett tillräckligt säkert lösenord, eller false om
+ * det inte uppfyller kraven.
+ *
+ * Varje lösenord ska console.log’as tillsammans med dess resultat.
+ *
+ *
+ * ROADMAP
+ *
+ * 1. Skapa en array som alla lösenord ligger i.
+ *
+ * 2.1. Flytta logiken som kollar om lösenordet uppfyller kraven till en egen
+ * funktion.
+ *
+ * 2.2. Kalla på funktionen en gång för varje lösenord i array:en och skicka
+ * med lösenordet till funktionen.
+ *
+ * 3. Ändra så att funktionen returnerar true/false istället för att logga till
+ * konsollen.
+ *
+ */
