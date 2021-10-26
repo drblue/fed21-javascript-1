897f9ff5ecd5410207c467817acfbdf923fe601d
Johan Nordström <johan@digitalvillage.se>
2021-10-26 14:58:25 +0200

    Arrow Functions, Callbacks and Array forEach.

--- 16-functions/script.js
+++ 16-functions/script.js
@@ -18,6 +18,7 @@
  * Function Expression
  */
 
+/*
 const greet = function() {
 	alert("Hello, World!");
 	console.log("Hello, World!");
@@ -39,3 +40,45 @@ const makeMoreInteresting = function(txt) {
 }
 
 console.log( makeMoreInteresting("☕️❓") )
+*/
+
+/**
+ * Arrow Function
+ * ("Fat Arrow Function")
+ */
+
+// Function Expression
+// const circleCircumferenceExpression = function(radius) {
+// 	return 2 * Math.PI * radius;
+// }
+
+// Function Expression (Arrow Function)
+// const circleCircumference = (radius) => {
+// 	return 2 * Math.PI * radius;
+// }
+
+/*
+const disturbUser = (txt) => {
+	alert(txt);
+}
+const nagUser = (txt) => {
+	console.log(txt);
+}
+
+const scream = (txt, apa) => {
+	let interesting = txt.toUpperCase() + "!!!!!";
+	apa(interesting);
+}
+
+scream("Is a bad movie", nagUser);
+*/
+
+let students = ["Johan", "Pelle", "Kajsa", "Maja"];
+
+for (let i = 0; i < students.length; i++) {
+	console.log(`Student at index ${i} is: ${students[i]}`);
+}
+
+students.forEach( (student, i) => {
+	console.log(`Student at index ${i} is: ${student}`);
+} );
