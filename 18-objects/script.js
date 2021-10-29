5ea5e25369225ee9cf612dbbd64f26107eefdc20
Johan Nordström <johan@digitalvillage.se>
2021-10-29 11:09:54 +0200

    Object Literals with attributes/properties.

--- 18-objects/script.js
+++ 18-objects/script.js
@@ -2,6 +2,9 @@
  * Objects
  */
 
+/*
+// Arrays aren't that suited for grouping related data together since they are index-based and relies on the order of values
+
 const fluffles = [
 	"Mr Fluffles",		// 0 = name
 	3,					// 1 = age
@@ -24,3 +27,52 @@ const showPetInfo = (pet) => {
 
 showPetInfo(fluffles);
 showPetInfo(captainCat);
+*/
+
+// Objects are better suited for representing data that has an inherent connection and is not dependent on the order of the keys
+
+// Object Literal Notation
+const fluffles = {
+	name: "Mr Fluffles",		// key-value pair
+	age: 3,
+	owner: "Bengt",
+	hobbies: "Annoy people",
+	species: "Cat"
+}
+
+const captainCat = {
+	name: "Captain Cat",
+	species: "Cat",
+	age: 9,
+	owner: "Agda",
+	hobbies: "HIIT",
+}
+
+const meowJr = {
+	hobbies: "Be cute",
+	species: "Kitten",
+	age: 1,
+	name: "Meow Jr"
+}
+
+
+// console.log(captainCat);
+
+// We access a property using dot-notation
+// console.log(fluffles.name);
+// console.log(captainCat.name);
+
+// square-bracket notation
+// console.log(fluffles['name']);
+// console.log(captainCat['name']);
+
+// If we try to access a property that does not exist on the object, we get `undefined`
+// console.log(meowJr.owner);
+
+const showPetInfo = (pet) => {
+	console.log(`${pet.name} is a ${pet.species} of ${pet.age} year(s) old. His owner is ${pet.owner} and his favorite hobby is to ${pet.hobbies}.`);
+}
+
+showPetInfo(fluffles);
+showPetInfo(captainCat);
+showPetInfo(meowJr);
