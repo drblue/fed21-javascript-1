5902c8fbfe44e00288bd83cc6533da326b10e0a3
Johan Nordström <johan@digitalvillage.se>
2021-11-02 13:59:14 +0100

    Add click-event listener for each listitem.

--- 23-dom-102/script.js
+++ 23-dom-102/script.js
@@ -6,3 +6,12 @@
 const listitemsEl = document.querySelector('ul');
 const listitems = document.querySelectorAll('li');
 const addButton = document.querySelector('#add');
+
+// forEach list item, add a event-listener for click-events
+listitems.forEach(listitem => {
+	// add a event-listener for click-events
+	listitem.addEventListener('click', (e) => {
+		console.log('oh you clicked on:', e.target);
+		e.target.style.color = "green"
+	})
+});
