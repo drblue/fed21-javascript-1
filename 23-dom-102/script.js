74f5174f4276e589800e0f62cac083c9f4b1b078
Johan Nordström <johan@digitalvillage.se>
2021-11-02 13:31:52 +0100

    DOM 102 boilerplate.

---
+++ 23-dom-102/script.js
@@ -0,0 +1,8 @@
+/**
+ * DOM - Document Object Model 102
+ *
+ */
+
+const listitemsEl = document.querySelector('ul');
+const listitems = document.querySelectorAll('li');
+const addButton = document.querySelector('#add');
