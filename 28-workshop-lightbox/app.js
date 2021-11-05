bfc6ffd12eb18209189cae9340fab072b097d97d
Johan Nordström <johan@digitalvillage.se>
2021-11-05 14:47:48 +0100

    Lightbox workshop template.

--- 28-workshop-lightbox/app.js
+++ 28-workshop-lightbox/app.js
@@ -1,4 +1,22 @@
 /**
  * Workshop: Lightbox
  *
+ * Två möjliga tillvägagångssätt
+ *
+ * 1a. Hämta ut alla `a.photo`, loopa över dem och lägg till en
+ * click-EventListener för varje element.
+ *
+ * 1b. Hämta ut `.photos`-elementet och lägg till en click-EventListener
+ * på den. I EventListener:n, kolla om det som klickades på är
+ * en IMG.
+ *
+ * 2. Hämta ut IMG's förälder (A-tagg) href och sätt på `#lightbox-wrapper img` src-attribut.
+ *
+ * 3. Lägg till klassen `show` på `#lightbox-wrapper`.
+ *
+ * 4. (Lägg till en click-EventListener på `#lightbox-wrapper` som tar bort `show`-klassen.)
+ *
+ * 5. 🌟 Om du kan fixa så man kan ha en caption på varje bild och den visas. Naturligtvis
+ * via JavaScript, lägg inte till en caption i själva bildfilen :P.
+ *
  */
