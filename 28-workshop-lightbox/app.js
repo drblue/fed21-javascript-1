972d2952792d1e1811c3644225d5d9037de82174
Johan Nordström <johan@digitalvillage.se>
2021-11-05 13:22:00 +0100

    Boilerplate for Lightbox-workshop.

---
+++ 28-workshop-lightbox/app.js
@@ -0,0 +1,94 @@
+/**
+ * Guess the number
+ *
+ * Skriv om ”gissa talet” till att ta emot och visa utfall i DOM. Använd
+ * formulär-fält för att ta emot input från användaren, och när formuläret
+ * skickas (submits) så jämför det gissade talet mot svaret och visa utfallet
+ * i DOM istället för alert()-rutor.
+ *
+ * STEG 1
+ * En input-box där man kan gissa på ett tal. En knapp för att gissa.
+ *
+ * STEG 1.1
+ * Visa resultatet i en alert.
+ *
+ * STEG 1.2
+ * Visa om resultatet var rätt eller inte i ett HTML-element.
+ *
+ * STEG 2
+ * Visa antalet gissningar hittills i ett HTML-element.
+ *
+ * STEG 3
+ * Visa om det gissade talet var för högt eller lågt i ett HTML-element.
+ *
+ * STEG 4
+ * Skapa en knapp för att starta om spelet (ett nytt tal ska slumpas fram och
+ * antalet gissningar ska nollställas).
+ *
+ */
+
+const cheatEl = document.querySelector('#cheat');
+const formGuessEl = document.querySelector('#formGuess');
+const inputGuessEl = document.querySelector('#inputGuess');
+const guessesEl = document.querySelector('#guesses');
+const turnoutEl = document.querySelector('#turnout');
+
+// Get a random number between 1-10
+const getRandomNumber = function(max = 10) {
+	return Math.ceil( Math.random() * max );
+}
+
+const updateGuesses = (guesses) => {
+	guessesEl.innerText = `${guesses} guesses`;
+}
+
+const startNewGame = () => {
+	// Get number to guess
+	correctNumber = getRandomNumber();
+
+	// Reset number of guesses to 0
+	guesses = 0;
+
+	// I'm going to cheat!
+	cheatEl.innerText = correctNumber;
+}
+
+let correctNumber;
+let guesses;
+
+formGuessEl.addEventListener('submit', e => {
+	// Stop form from being sent to the server
+	e.preventDefault();
+
+	// Get guessed number from input-field (and convert it to a Number)
+	const guessedNumber = Number(inputGuessEl.value);
+
+	// Increase guesses made
+	guesses++;
+
+	// Update DOM with guesses made
+	updateGuesses(guesses);
+
+	// Check if guessedNumber is correct
+	if (guessedNumber === correctNumber) {
+		turnoutEl.innerText = `${guessedNumber} is correct!`;
+	} else if (guessedNumber < correctNumber) {
+		turnoutEl.innerText = `${guessedNumber} is too low`;
+	} else if (guessedNumber > correctNumber){
+		turnoutEl.innerText = `${guessedNumber} is too high`;
+	}
+});
+
+formGuessEl.addEventListener('reset', e => {
+	// Start new game
+	startNewGame();
+
+	// Update DOM with guesses made
+	updateGuesses(guesses);
+
+	// Empty previous result
+	turnoutEl.innerText = "";
+});
+
+// Start initial game
+startNewGame();
