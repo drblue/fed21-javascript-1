994965ddcf466c5b4a2deafd45091b19eb4b1a6f
Johan Nordström <johan@digitalvillage.se>
2021-11-09 10:04:18 +0100

    Lightbox-workshop step 1b-4.

--- 28-workshop-lightbox/app.js
+++ 28-workshop-lightbox/app.js
@@ -25,6 +25,7 @@ const lightboxWrapperEl = document.querySelector('#lightbox-wrapper');
 const lightboxImgEl = document.querySelector('#lightbox-wrapper img');
 const lightboxCaptionEl = document.querySelector('#lightbox-wrapper .caption');
 
+/*
 // 1a. Get all `a.photo`, loop over them and add a
 // click-eventlistener to each element
 document.querySelectorAll('a.photo').forEach(link => {
@@ -46,4 +47,32 @@ document.querySelectorAll('a.photo').forEach(link => {
 		// finally, add class `show` to `#lightbox-wrapper`
 		lightboxWrapperEl.classList.add('show');
 	})
+});
+*/
+
+// 1b. Get `.photos`-element and add a click-eventlistener to it
+document.querySelector('.photos').addEventListener('click', e => {
+	// prevent default action for clicks
+	e.preventDefault();
+
+	// check if the element that was clicked on is a IMG
+	if (e.target.tagName === "IMG") {
+		// retrieve URL to large image from parent element's href-attribute
+		const large_img_url = e.target.parentElement.getAttribute('href');
+
+		// set URL to large image on lightbox's image-element's src
+		lightboxImgEl.setAttribute('src', large_img_url);
+
+		// finally, add class `show` to `#lightbox-wrapper`
+		lightboxWrapperEl.classList.add('show');
+	}
+});
+
+// Add click-eventlistener to `#lightbox-wrapper` and remove class `show`
+lightboxWrapperEl.addEventListener('click', () => {
+	// remove class `show` from lightboxWrapperEl
+	lightboxWrapperEl.classList.remove('show');
+
+	// empty lightboxImg src-attribute
+	lightboxImgEl.setAttribute('src', '');
 });
