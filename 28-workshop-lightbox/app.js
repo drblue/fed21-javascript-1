f18c4830c57d8728c8ebd12530e28cac468bc36a
Johan Nordström <johan@digitalvillage.se>
2021-11-09 09:48:59 +0100

    Lightbox-workshop step 1a-3.

--- 28-workshop-lightbox/app.js
+++ 28-workshop-lightbox/app.js
@@ -20,3 +20,30 @@
  * via JavaScript, lägg inte till en caption i själva bildfilen :P.
  *
  */
+
+const lightboxWrapperEl = document.querySelector('#lightbox-wrapper');
+const lightboxImgEl = document.querySelector('#lightbox-wrapper img');
+const lightboxCaptionEl = document.querySelector('#lightbox-wrapper .caption');
+
+// 1a. Get all `a.photo`, loop over them and add a
+// click-eventlistener to each element
+document.querySelectorAll('a.photo').forEach(link => {
+	link.addEventListener('click', e => {
+		// stop browser from following the link
+		// (and hence leaving the page)
+		e.preventDefault();
+
+		// console.log("You clicked on:", e.target);
+		// console.log("That elements parent is:", e.target.parentElement);
+		// console.log("Parents href is:", e.target.parentElement.getAttribute('href'));
+
+		// retrieve URL to large image from parent element's href-attribute
+		const large_img_url = e.target.parentElement.getAttribute('href');
+
+		// set URL to large image on lightbox's image-element's src
+		lightboxImgEl.setAttribute('src', large_img_url);
+
+		// finally, add class `show` to `#lightbox-wrapper`
+		lightboxWrapperEl.classList.add('show');
+	})
+});
