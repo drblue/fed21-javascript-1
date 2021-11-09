7e6b4d8c02bb71bd1d5df73f812129b5635fe927
Johan Nordström <johan@digitalvillage.se>
2021-11-09 11:15:49 +0100

    Array sorting (part 1).

--- 29-array-methods/sort.js
+++ 29-array-methods/sort.js
@@ -7,3 +7,28 @@
  * .map()
  * .reduce()
  */
+
+// names
+/*
+const names = ["Johan", "Fredrik", "Peter", "Alicia", "Maja", "Elliot"];
+console.log("Names in chaos:", names);
+
+// sort array alphabetically
+names.sort();
+
+console.log("Names in proper order:", names);
+
+// reverse order
+names.reverse();
+console.log("Names in reverse order:", names);
+*/
+
+// numbers
+const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 7, 33, 52 ];
+
+console.log("Numbers in chaos:", numbers);
+
+// sort numbers
+numbers.sort();
+
+console.log("Numbers in proper? order:", numbers);
