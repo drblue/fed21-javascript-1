221be5437b78df4a90f810ae2f1dd6b508730b3a
Johan Nordström <johan@digitalvillage.se>
2021-11-10 13:15:27 +0100

    Mini-workshop using array-methods.

--- 29-array-methods/map.js
+++ 29-array-methods/map.js
@@ -68,3 +68,24 @@ const sale_products = products.map(product => {
 		price: product.price / 2
 	};
 });
+
+// school friends
+const friends = [
+	{ name: 'John', grade: 2, year: 3, sex: 'M' },
+	{ name: 'Sarah', grade: 3, year: 2, sex: 'F' },
+	{ name: 'Bob', grade: 3, year: 5, sex: 'M' },
+	// { grade: 1, year: 42, sex: 'Alien' },
+	{ name: 'Johnny', grade: 2, year: 2, sex: 'M' },
+	{ name: 'Ethan', grade: 4, year: 1, sex: 'M' },
+	{ name: 'Paula', grade: 4, year: 5, sex: 'F' },
+	{ name: 'Donald', grade: 5, year: 5, sex: 'M' },
+	{ name: 'Jennifer', grade: 3, year: 3, sex: 'F' },
+	{ name: 'Courtney', grade: 3, year: 1, sex: 'F' },
+	{ name: 'Jane', grade: 4, year: 3, sex: 'F '}
+];
+
+// USE ONLY THE NEW ARRAY METHODS (sort/filter/find/map)
+
+// 1. get an array of the names of all friends in year 3
+
+// 2. get an array of the names of all male friends in year 5
