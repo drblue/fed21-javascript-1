5b49a6af5b557914c7517df39c023ed72040bd0e
Johan Nordström <johan@digitalvillage.se>
2021-11-10 14:22:07 +0100

    Array reduce boilerplate.

---
+++ 29-array-methods/reduce.js
@@ -0,0 +1,75 @@
+/**
+ * Array Methods
+ *
+ * .sort()
+ * .filter()
+ * .find()
+ * .map()
+ * .reduce() <--
+ */
+
+/**
+ * Numbers
+ */
+const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 5, 33 ];
+
+
+
+/**
+ * Students
+ */
+const students = [
+	{
+		name: "Johan",
+		points: 133,
+	},
+	{
+		name: "Peter",
+		points: 3,
+	},
+	{
+		name: "Alicia",
+		points: 42,
+	},
+	{
+		name: "Elliot",
+		points: 88,
+	},
+	{
+		name: "Maja",
+		points: 35,
+	},
+];
+
+
+
+/**
+ * Products
+ */
+const products = [
+	{
+		sku: "CORR-BWL",
+		name: "Corrosive bowl",
+		in_stock: 321,
+		price: 0.99,
+	},
+	{
+		sku: "CTN-SPCE",
+		name: "Cotton spice rack",
+		in_stock: 2,
+		price: 149.99,
+	},
+	{
+		sku: "GOOD-COOKIES",
+		name: "Inside-out Oreo cookies",
+		in_stock: 18,
+		price: 2.49,
+	},
+	{
+		sku: "BACK-BREAKER",
+		name: "The uncomfortable broom",
+		in_stock: 1,
+		price: 28.65,
+	},
+];
+
