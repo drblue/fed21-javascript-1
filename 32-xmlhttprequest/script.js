d195b81e3de7148b7515c6451791abd7a3a9ffd2
Johan Nordström <johan@digitalvillage.se>
2021-11-17 09:58:11 +0100

    Create a request and send it.

--- 32-xmlhttprequest/script.js
+++ 32-xmlhttprequest/script.js
@@ -2,3 +2,20 @@
  * XMLHttpRequest
  *
  */
+
+// Create a new XML Http Request
+const request = new XMLHttpRequest();
+
+// Attach a event listener to the request
+request.addEventListener('readystatechange', () => {
+	// Check status of request
+});
+
+// Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
+request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+// Send request
+request.send();
+
+// Done?
+console.log("Request sent!");
