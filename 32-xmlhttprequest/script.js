8ecc6e997c320f80b05fc1dc01c64f10479222e4
Johan Nordström <johan@digitalvillage.se>
2021-11-17 10:52:04 +0100

    Listen for when the request is DONE, and check status code of response. If response is 200 OK, parse the content of the response from JSON into something we can use.

--- 32-xmlhttprequest/script.js
+++ 32-xmlhttprequest/script.js
@@ -8,7 +8,23 @@ const request = new XMLHttpRequest();
 
 // Attach a event listener to the request
 request.addEventListener('readystatechange', () => {
-	// Check status of request
+	// Check if readyState is 4 (=== DONE)
+	if (request.readyState === 4) {
+
+		// Was request successful (200 = OK)?
+		if (request.status === 200) {
+			// Request returned successful!
+			console.log("Request was OK");
+
+			// take a STRING and PARSE it into a JavaScript Object (Array)
+			const data = JSON.parse(request.responseText);
+			console.log("Data:", data);
+
+		} else {
+			// Something went wrong with the request
+			console.log("Request was *NOT* OK!");
+		}
+	}
 });
 
 // Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
