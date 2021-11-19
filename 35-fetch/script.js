dfec8e2e70f97a9500b6a48db2a6fc7768789bff
Johan Nordström <johan@digitalvillage.se>
2021-11-19 14:37:36 +0100

    Trigger error if fetch didn't result in 200 OK.

--- 35-fetch/script.js
+++ 35-fetch/script.js
@@ -3,3 +3,29 @@
  *
  */
 
+/*
+fetch('data/dogs.json')
+	.then(res => res.json())
+	.then(data => {
+		console.log("Got data!", data);
+	})
+	.catch(err => {
+		console.log("Dogs rejected!", err);
+	});
+*/
+
+fetch('data/horses.json')
+	.then(res => {
+		// trigger an error
+		if (!res.ok) {
+			throw new Error("Response was not OK!");
+		}
+
+		return res.json();
+	})
+	.then(data => {
+		console.log("Got data!", data);
+	})
+	.catch(err => {
+		console.log("Dogs rejected!", err);
+	});
