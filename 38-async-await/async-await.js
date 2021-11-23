e9c4f97281c5cb00433317eb9b62aadd5e5d7ea9
Johan Nordström <johan@digitalvillage.se>
2021-11-23 11:17:43 +0100

    async/await boilerplate.

---
+++ 38-async-await/async-await.js
@@ -0,0 +1,14 @@
+/**
+ * async/await
+ */
+
+/*
+const postsPromise = fetch('https://jsonplaceholder.typicode.com/posts')
+	.then(res => res.json())
+	.then(posts => {
+		// output posts
+		document.querySelector('#posts').innerHTML = posts
+			.map(post => `<li>${post.title}</li>`)
+			.join('');
+	});
+*/
